2684f9a2948eceb2de519fb6460f0cbd4c2fe8c1
jerapiblaze <tddt0310@gmail.com>
2024-07-22 16:04:00 +0700

    fix script

--- runall.sh
+++ runall.sh
@@ -1,9 +1,9 @@
 #!/bin/zsh
 
-python GenerateProblems.py configs/ProblemSettings/dummy.yaml
-python GenerateProblems.py configs/ProblemSettings/dummy_c1.yaml
-python GenerateProblems.py configs/ProblemSettings/dummy_c3.yaml
+python GenerateProblems.py -c configs/ProblemSettings/dummy.yaml
+python GenerateProblems.py -c configs/ProblemSettings/dummy_c1.yaml
+python GenerateProblems.py -c configs/ProblemSettings/dummy_c3.yaml
 
-python SolveProblems.py configs/SolveSettings/dummy.yaml
-python SolveProblems.py configs/SolveSettings/dummy_c1.yaml
-python SolveProblems.py configs/SolveSettings/dummy_c3.yaml
+python SolveProblems.py -c configs/SolveSettings/dummy.yaml
+python SolveProblems.py -c configs/SolveSettings/dummy_c1.yaml
+python SolveProblems.py -c configs/SolveSettings/dummy_c3.yaml
