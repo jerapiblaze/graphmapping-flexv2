7ba5f27b5ad36cd9d79413d583a4f4cd22d2d4f1
j12t <minhthanh.31oct2k3@gmail.com>
2025-11-27 12:30:04 +0700

    add more config

--- FlexSliceMappingProblem/slice/flex.py
+++ FlexSliceMappingProblem/slice/flex.py
@@ -12,14 +12,16 @@ class FlexSliceGenerator(SliceGenerator):
     def Generate(self) -> list[nx.DiGraph]:
         name = f"flexslice_{uuid.uuid4().hex[:8]}"
         slice_configs = []
-        if self.mode.__contains__("_1_"):
+        if self.mode.__contains__("_1_"): #k1
                 slice_configs.append(GenerateConfig_1(name))
-        if self.mode.__contains__("_2_"):
+        if self.mode.__contains__("_2_"): # not used
                 slice_configs.append(GenerateConfig_2(name))
-        if self.mode.__contains__("_3_"):
+        if self.mode.__contains__("_3_"): #k2
                 slice_configs.append(GenerateConfig_3(name))
-        if self.mode.__contains__("_4_"):
+        if self.mode.__contains__("_4_"): # not used
                 slice_configs.append(GenerateConfig_4(name))
+        if self.mode.__contains__("_5_"): #k3
+                slice_configs.append(GenerateConfig_5(name))
         return slice_configs
     
     def GenerateSet(self, n:int) -> list[list[nx.DiGraph]]:
@@ -260,6 +262,59 @@ def GenerateConfig_4(prefix:str) -> nx.DiGraph:
         )
     ]
     graph = nx.DiGraph(name=f"{prefix}_type4_{uuid.uuid4().hex[:8]}")
+    graph.add_nodes_from(NODES)
+    graph.add_edges_from(LINKS)
+    return graph
+
+def GenerateConfig_5(prefix:str) -> nx.DiGraph:
+    NODES = [
+        (
+            0, {
+                "label":"VPN",
+                "name":"VirtualPersonalNetwork",
+                "req": NodeResource(10, 0, 0)
+            }
+        ),
+        (
+            1, {
+                "label":"MN",
+                "name":"Monitoring",
+                "req": NodeResource(5, 0, 0)
+            }
+        ),
+        (
+            2, {
+                "label":"FW",
+                "name":"Firewall",
+                "req": NodeResource(10, 0, 0)
+            }
+        ),
+        (
+            3, {
+                "label":"LB",
+                "name":"LoadBalancer",
+                "req": NodeResource(20, 0, 0)
+            }
+        )
+    ]
+    LINKS = [
+        (
+            0, 2, {
+                "req": LinkResource(15)
+            }
+        ),
+        (
+            2, 1 , {
+                "req": LinkResource(10)
+            }
+        ),
+        (
+            2, 3 , {
+                "req": LinkResource(10)
+            }
+        )
+    ]
+    graph = nx.DiGraph(name=f"{prefix}_type3_{uuid.uuid4().hex[:8]}")
     graph.add_nodes_from(NODES)
     graph.add_edges_from(LINKS)
     return graph
